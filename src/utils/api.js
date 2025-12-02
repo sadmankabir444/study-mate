5f5a047c1b4cee0f9e1c5bf89935c3b875badab2
Sadman kabir <sadmankabir351@gmail.com>
2025-12-02 17:52:21 +0600

    added icon

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,17 @@
-import axios from 'axios'
+// import axios from 'axios'
 
 
-const api = axios.create({
-baseURL: import.meta.env.VITE_API_BASE || '/',
-headers: { 'Content-Type': 'application/json' }
-})
+// const api = axios.create({
+// baseURL: import.meta.env.VITE_API_BASE || '/',
+// headers: { 'Content-Type': 'application/json' }
+// })
 
 
-export async function getTopPartners(){
-// expects endpoint /api/partners/top that returns an array of partner objects
-const res = await api.get('/api/partners/top')
-return res.data
-}
+// export async function getTopPartners(){
+// // expects endpoint /api/partners/top that returns an array of partner objects
+// const res = await api.get('/api/partners/top')
+// return res.data
+// }
 
 
-export default api
+// export default api
